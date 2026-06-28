1cb03d822aed35d0cb59ae9733e3ea83a8ec987d
CauandoP <148398651+ocauandev@users.noreply.github.com>
2026-06-28 14:08:05 -0300

    feat: implementa a interface IPista

--- src/interfaces/IPista.java
+++ src/interfaces/IPista.java
@@ -1,4 +1,29 @@
 package interfaces;
 
 public interface IPista {
+    /**
+     * Retorna o número/ID identificador da pista.
+     */
+    int getIdPista();
+
+    /**
+     * Altera o estado interno da pista para ocupada pelo avião informado.
+     * Contém a asserção contra Race Conditions.
+     */
+    void alocarPista(IAviao aviao);
+
+    /**
+     * Libera o estado da pista, limpando o avião atual.
+     */
+    void desocuparPista();
+
+    /**
+     * Retorna se a pista está sendo utilizada por alguma thread no momento.
+     */
+    boolean estaOcupada();
+
+    /**
+     * Retorna o nome do arquivo texto associado ao status desta pista[cite: 20].
+     */
+    String getNomeArquivoStatus();
 }
