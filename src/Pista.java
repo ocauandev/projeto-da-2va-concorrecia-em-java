3f31f7db0750ffbd8f3c9af3971146425922b60e
CauandoP <148398651+ocauandev@users.noreply.github.com>
2026-06-28 15:47:34 -0300

    feat: implementa a classe Pista com assertions de exclusão mútua

--- src/Pista.java
+++ src/Pista.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import interfaces.IPista;
+import interfaces.IAviao;
 
-public class Pista {
+public class Pista implements IPista {
+    private final int idPista;
+    private boolean ocupada;
+    private IAviao aviaoAtual;
+    private final String nomeArquivoStatus;
+
+    public Pista(int idPista) {
+        this.idPista = idPista;
+        this.ocupada = false;
+        this.aviaoAtual = null;
+        this.nomeArquivoStatus = "status_pista_" + idPista + ".txt"; // [cite: 20]
+    }
+
+    @Override
+    public int getIdPista() { return this.idPista; }
+
+    @Override
+    public synchronized void alocarPista(IAviao aviao) {
+        // [ZONA DE ASSERÇÃO CRÍTICA] Exigência do PDF 
+        // Se duas threads entrarem aqui simultaneamente devido a falhas na torre, o assert dispara.
+        assert !this.ocupada : "RACE CONDITION DETECTADA: Pista " + idPista + " invadida por " + aviao.getIdVoo() + " enquanto ocupada!";
+
+        this.ocupada = true;
+        this.aviaoAtual = aviao;
+    }
+
+    @Override
+    public synchronized void desocuparPista() {
+        this.ocupada = false;
+        this.aviaoAtual = null;
+    }
+
+    @Override
+    public synchronized boolean estaOcupada() { return this.ocupada; }
+
+    @Override
+    public String getNomeArquivoStatus() { return this.nomeArquivoStatus; }
 }
